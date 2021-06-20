b1bea4be69f24c28bb25b5a753112e201fd391ad
atopilski <atopilski@gmail.com>
2021-06-20 00:58:54 -0400

    Background effect

---
+++ include/fastotv/types/ml/background_effect.h
@@ -0,0 +1,65 @@
+/*  Copyright (C) 2014-2021 FastoGT. All right reserved.
+    This file is part of fastocloud.
+    fastocloud is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    fastocloud is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with fastocloud.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <string>
+
+#include <common/serializer/json_serializer.h>
+#include <common/value.h>
+
+namespace fastotv {
+namespace ml {
+
+enum BackgroundEffectType { BLUR = 0, IMAGE, COLOR };
+
+class BackgroundEffect : public common::serializer::JsonSerializer<BackgroundEffect> {
+ public:
+  typedef common::serializer::JsonSerializer<BackgroundEffect> base_class;
+  typedef common::Optional<double> blur_strength_t;
+  typedef common::Optional<std::string> image_path_t;
+  typedef common::Optional<std::string> color_t;
+
+  static BackgroundEffect MakeBlurEffect(blur_strength_t blur);        // for blur type
+  static BackgroundEffect MakeImageEffect(const image_path_t& image);  // for image type
+  static BackgroundEffect MakeColorEffect(const color_t& color);       // for color type
+
+  ~BackgroundEffect() override;
+
+  bool Equals(const BackgroundEffect& back) const;
+
+  BackgroundEffectType GetType() const;
+  blur_strength_t GetBlurStrength() const;
+  image_path_t GetImagePath() const;
+  color_t GetColor() const;
+
+  static common::Optional<BackgroundEffect> Make(common::HashValue* hash);
+
+ protected:
+  common::Error DoDeSerialize(json_object* serialized) override;
+  common::Error SerializeFields(json_object* out) const override;
+
+ private:
+  BackgroundEffect(BackgroundEffectType type);
+
+  BackgroundEffectType type_;
+
+  // should be union but not trivial types
+  blur_strength_t blur_;
+  image_path_t image_;
+  color_t color_;
+};
+
+}  // namespace ml
+}  // namespace fastotv
---
+++ src/types/ml/background_effect.cpp
@@ -0,0 +1,154 @@
+/*  Copyright (C) 2014-2019 FastoGT. All right reserved.
+    This file is part of fastocloud.
+    fastocloud is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    fastocloud is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with fastocloud.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <fastotv/types/ml/background_effect.h>
+
+#include <common/sprintf.h>
+
+#define TYPE_FIELD "type"
+#define STRENGTH_FIELD "strength"
+#define IMAGE_FIELD "path"
+#define COLOR_FIELD "color"
+
+namespace fastotv {
+namespace ml {
+
+BackgroundEffect::BackgroundEffect(BackgroundEffectType type) : type_(type), blur_(), image_(), color_() {}
+
+BackgroundEffect BackgroundEffect::MakeBlurEffect(blur_strength_t blur) {
+  auto result = BackgroundEffect(BLUR);
+  result.blur_ = blur;
+  return result;
+}
+
+BackgroundEffect BackgroundEffect::MakeImageEffect(const image_path_t& image) {
+  auto result = BackgroundEffect(BLUR);
+  result.image_ = image;
+  return result;
+}
+
+BackgroundEffect BackgroundEffect::MakeColorEffect(const color_t& color) {
+  auto result = BackgroundEffect(BLUR);
+  result.color_ = color;
+  return result;
+}
+
+bool BackgroundEffect::Equals(const BackgroundEffect& back) const {
+  return back.type_ == type_;
+}
+
+BackgroundEffectType BackgroundEffect::GetType() const {
+  return type_;
+}
+
+BackgroundEffect::blur_strength_t BackgroundEffect::GetBlurStrength() const {
+  return blur_;
+}
+
+BackgroundEffect::image_path_t BackgroundEffect::GetImagePath() const {
+  return image_;
+}
+
+BackgroundEffect::color_t BackgroundEffect::GetColor() const {
+  return color_;
+}
+
+common::Optional<BackgroundEffect> BackgroundEffect::Make(common::HashValue* hash) {
+  if (!hash) {
+    return common::Optional<BackgroundEffect>();
+  }
+
+  common::Value* type_field = hash->Find(TYPE_FIELD);
+  int type;
+  if (!type_field || !type_field->GetAsInteger(&type)) {
+    return common::Optional<BackgroundEffect>();
+  }
+
+  if (type == BLUR) {
+    double blur;
+    common::Value* strength_field = hash->Find(STRENGTH_FIELD);
+    if (!strength_field || !strength_field->GetAsDouble(&blur)) {
+      return common::Optional<BackgroundEffect>();
+    }
+    return MakeBlurEffect(blur);
+  } else if (type == IMAGE) {
+    std::string path;
+    common::Value* path_field = hash->Find(IMAGE_FIELD);
+    if (!path_field || !path_field->GetAsBasicString(&path)) {
+      return common::Optional<BackgroundEffect>();
+    }
+    return MakeImageEffect(path);
+  } else if (type == COLOR) {
+    std::string color;
+    common::Value* color_field = hash->Find(COLOR_FIELD);
+    if (!color_field || !color_field->GetAsBasicString(&color)) {
+      return common::Optional<BackgroundEffect>();
+    }
+    return MakeColorEffect(color);
+  }
+  return common::Optional<BackgroundEffect>();
+}
+
+common::Error BackgroundEffect::DoDeSerialize(json_object* serialized) {
+  BackgroundEffectType type;
+  common::Error err = GetEnumField(serialized, TYPE_FIELD, &type);
+  if (err) {
+    return err;
+  }
+
+  if (type == BLUR) {
+    double blur;
+    err = GetDoubleField(serialized, STRENGTH_FIELD, &blur);
+    if (err) {
+      return err;
+    }
+    *this = MakeBlurEffect(blur);
+    return common::Error();
+  } else if (type == IMAGE) {
+    std::string image;
+    err = GetStringField(serialized, IMAGE_FIELD, &image);
+    if (err) {
+      return err;
+    }
+    *this = MakeImageEffect(image);
+    return common::Error();
+  } else if (type == COLOR) {
+    std::string color;
+    err = GetStringField(serialized, COLOR_FIELD, &color);
+    if (err) {
+      return err;
+    }
+    *this = MakeColorEffect(color);
+    return common::Error();
+  }
+
+  return common::make_error(common::MemSPrintf("unknown type: %d", type));
+}
+
+common::Error BackgroundEffect::SerializeFields(json_object* out) const {
+  ignore_result(SetIntField(out, TYPE_FIELD, type_));
+  if (blur_) {
+    ignore_result(SetDoubleField(out, STRENGTH_FIELD, *blur_));
+  }
+  if (image_) {
+    ignore_result(SetStringField(out, IMAGE_FIELD, *image_));
+  }
+  if (color_) {
+    ignore_result(SetStringField(out, COLOR_FIELD, *color_));
+  }
+  return common::Error();
+}
+
+}  // namespace ml
+}  // namespace fastotv
