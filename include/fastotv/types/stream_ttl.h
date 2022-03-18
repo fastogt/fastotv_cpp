92edd695fc11ff43901bd2dbfede3b1840583847
topilski <atopilski@gmail.com>
2022-03-18 00:44:27 -0600

    Stream TTL

---
+++ include/fastotv/types/stream_ttl.h
@@ -0,0 +1,61 @@
+/*  Copyright (C) 2014-2022 FastoGT. All right reserved.
+    This file is part of fastocloud.
+    fastocloud is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    fastocloud is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with fastocloud.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <string>
+
+#include <common/serializer/json_serializer.h>
+#include <common/value.h>
+
+namespace fastotv {
+
+class StreamTTL : public common::serializer::JsonSerializer<StreamTTL> {
+ public:
+  typedef JsonSerializer<StreamTTL> base_class;
+  typedef common::utctime_t ttl_t;
+
+  StreamTTL();
+  explicit StreamTTL(const ttl_t& ttl, bool phoenix);
+
+  bool IsValid() const;
+
+  ttl_t GetTTL() const;
+  void SetTTL(const ttl_t& ttl);
+
+  void SetPhoenix(bool phoenix);
+  bool GetPhoenix() const;
+
+  bool Equals(const StreamTTL& key) const;
+
+  static common::Optional<StreamTTL> Make(common::HashValue* json);
+
+ protected:
+  common::Error DoDeSerialize(json_object* serialized) override;
+  common::Error SerializeFields(json_object* out) const override;
+
+ private:
+  ttl_t ttl_;
+  bool phoenix_;
+};
+
+inline bool operator==(const StreamTTL& left, const StreamTTL& right) {
+  return left.Equals(right);
+}
+
+inline bool operator!=(const StreamTTL& x, const StreamTTL& y) {
+  return !(x == y);
+}
+
+}  // namespace fastotv
---
+++ src/types/stream_ttl.cpp
@@ -0,0 +1,102 @@
+/*  Copyright (C) 2014-2022 FastoGT. All right reserved.
+    This file is part of fastocloud.
+    fastocloud is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    fastocloud is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with fastocloud.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <fastotv/types/stream_ttl.h>
+
+#define TTL_FIELD "ttl"
+#define PHOENIX_FIELD "pbkeylen"
+
+namespace fastotv {
+
+StreamTTL::StreamTTL() : ttl_(0), phoenix_(false) {}
+
+StreamTTL::StreamTTL(const ttl_t& ttl, bool phoenix) : ttl_(ttl), phoenix_(phoenix) {}
+
+bool StreamTTL::IsValid() const {
+  return true;
+}
+
+StreamTTL::ttl_t StreamTTL::GetTTL() const {
+  return ttl_;
+}
+
+void StreamTTL::SetTTL(const ttl_t& ttl) {
+  ttl_ = ttl;
+}
+
+void StreamTTL::SetPhoenix(bool phoenix) {
+  phoenix_ = phoenix;
+}
+
+bool StreamTTL::GetPhoenix() const {
+  return phoenix_;
+}
+
+bool StreamTTL::Equals(const StreamTTL& key) const {
+  return phoenix_ == key.phoenix_ && ttl_ == key.ttl_;
+}
+
+common::Optional<StreamTTL> StreamTTL::Make(common::HashValue* json) {
+  if (!json) {
+    return common::Optional<StreamTTL>();
+  }
+
+  StreamTTL res;
+  common::Value* pass_field = json->Find(TTL_FIELD);
+  ttl_t ttl;
+  if (!pass_field || !pass_field->GetAsTime(&ttl)) {
+    return common::Optional<StreamTTL>();
+  }
+  res.ttl_ = ttl;
+
+  bool phoenix;
+  common::Value* kl_field = json->Find(PHOENIX_FIELD);
+  if (!kl_field || !kl_field->GetAsBoolean(&phoenix)) {
+    return common::Optional<StreamTTL>();
+  }
+  res.phoenix_ = phoenix;
+  return res;
+}
+
+common::Error StreamTTL::DoDeSerialize(json_object* serialized) {
+  StreamTTL res;
+  ttl_t ttl;
+  common::Error err = GetInt64Field(serialized, TTL_FIELD, &ttl);
+  if (err) {
+    return err;
+  }
+  res.ttl_ = ttl;
+
+  bool phoenix;
+  err = GetBoolField(serialized, PHOENIX_FIELD, &phoenix);
+  if (err) {
+    return err;
+  }
+
+  res.phoenix_ = phoenix;
+  *this = res;
+  return common::Error();
+}
+
+common::Error StreamTTL::SerializeFields(json_object* out) const {
+  if (!IsValid()) {
+    return common::make_error_inval();
+  }
+
+  ignore_result(SetInt64Field(out, TTL_FIELD, ttl_));
+  ignore_result(SetBoolField(out, PHOENIX_FIELD, phoenix_));
+  return common::Error();
+}
+
+}  // namespace fastotv
